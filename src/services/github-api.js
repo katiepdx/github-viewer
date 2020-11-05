39f00a4a2b113a6d09c2d2b5fc5b921fac8f0def
katiePDX <61799774+katiepdx@users.noreply.github.com>
2020-11-04 16:28:11 -0800

    update api fetch

--- src/services/github-api.js
+++ src/services/github-api.js
@@ -1,18 +1,14 @@
 // github api fetch - will name in username from search bar
 
 export const getUserInfo = async (search = 'github') => {
-  // USER
-  const userRes = await fetch(`https://api.github.com/users/${search}`)
-
-  if (!userRes.ok) return ('failed to fetch user from api')
-
-  const user = await userRes.json()
-
-  // REPOS
+  // USER and REPOS fetch
   const reposRes = await fetch(`https://api.github.com/users/${search}/repos`)
-
+  const userRes = await fetch(`https://api.github.com/users/${search}`)
+  
   if (!reposRes.ok) return ('failed to fetch repos from api')
-
+  if (!userRes.ok) return ('failed to fetch user from api')
+  
+  const user = await userRes.json()
   const repos = await reposRes.json()
 
   // USER INFO object
